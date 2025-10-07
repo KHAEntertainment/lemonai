c356262f220dd0c0b721051fc0a51aac101dcc30
Billy Brenner <43256680+KHAEntertainment@users.noreply.github.com>
2025-10-07 01:10:28 -0700

    Update vite.config.js
    adjusted to allow for reverse proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,44 +8,47 @@ import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd()); // ✅ 根据 --mode 自动加载正确的 .env 文件
-
   return {
-  root: __dirname,
-  // 根据环境变量 VITE_IS_CLIENT 决定使用哪个 base 
-  base: env.VITE_IS_CLIENT === 'true' ? './' : '/', 
-  build: {
-    outDir: '../.vite/renderer/main_window', // 输出到 Electron Forge 期望的目录
-    emptyOutDir: false
-  },
-  plugins: [
-    vue(),
-    svgLoader(),
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: 'less',
-        }),
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '~@': path.resolve(__dirname, 'src'),
+    root: __dirname,
+    // 根据环境变量 VITE_IS_CLIENT 决定使用哪个 base 
+    base: env.VITE_IS_CLIENT === 'true' ? './' : '/', 
+    build: {
+      outDir: '../.vite/renderer/main_window', // 输出到 Electron Forge 期望的目录
+      emptyOutDir: false
     },
-  },
-  server: {
-    port: env.VITE_PORT || 5005,
-    host: '0.0.0.0',
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: env.VITE_SERVICE_URL || 'http://127.0.0.1:3000',
-        protocol: 'http',
-        changeOrigin: true,
-        ws: true,
+    plugins: [
+      vue(),
+      svgLoader(),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: 'less',
+          }),
+        ],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '~@': path.resolve(__dirname, 'src'),
       },
     },
+    server: {
+      port: env.VITE_PORT || 5173,
+      host: '0.0.0.0',
+      strictPort: true,
+      hmr: {
+        protocol: 'wss',
+        clientPort: 443,
+      },
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVICE_URL || 'http://127.0.0.1:3000',
+          protocol: 'http',
+          changeOrigin: true,
+          ws: true,
+        },
+      },
     },
   };
 });
